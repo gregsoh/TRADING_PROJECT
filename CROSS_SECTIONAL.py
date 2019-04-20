884024141cf3d07b8718645e921c844140900c31
gregsoh <46077253+gregsoh@users.noreply.github.com>
2019-04-20 00:24:11 -0700

    Updated

--- CROSS_SECTIONAL.py
+++ CROSS_SECTIONAL.py
@@ -6,19 +6,12 @@ import numpy as np
 from pandas_datareader import data
 from sklearn.linear_model import LinearRegression
 
-def download():
-	# American Airlines, Delta Airlines, Spirit Airlines, SouthWest Airlines
-	AAL, DAL, SAVE, LUV = {}, {}, {}, {}
-	ticker = ["AAL", "DAL", "SAVE", "LUV"]
-	
-	start_date = '2016-12-01'
-	end_date = '2016-12-31'
-	pred = []
-	for tick in ticker:
-		pd = data.DataReader(tick, 'yahoo', start_date, end_date)
-		t = pd["Close"].values #temporary
-		pred.append(np.reshape(t, (len(t), 1)))
-	
+def download(ticker, start_date, end_date):
+	pd = data.DataReader(ticker, 'yahoo', start_date, end_date)
+	t = pd["Close"].values #temporary
+	return np.reshape(t, (len(t), 1))
+
+def factors(start_date, end_date):
 	# Oil stocks: https://www.fool.com/investing/2018/09/06/the-5-biggest-oil-stocks-in-the-us.aspx
 	F1 = data.DataReader("XOM", 'yahoo', start_date, end_date)["Close"].values
 	F1 = np.reshape(F1, (len(F1), 1))
@@ -42,11 +35,26 @@ def download():
 	F7 = data.DataReader("AB", 'yahoo', start_date, end_date)["Close"].values
 	F7 = np.reshape(F7, (len(F7), 1))
 	v = np.hstack((v, F7))
-	return pred, v
+	return v
+
+def loss(start_date, end_date, ticker, reg):
+	f = factors(start_date, end_date)
+	p = download(ticker, start_date, end_date)
+	return reg.score(f, p)
 
 def linearRegression():
-	p, v = download()
-	for _ in p:
-		reg = LinearRegression().fit(v, _)
-		print(reg.score(v, _), reg.coef_)
-linearRegression()
+	# American Airlines, Delta Airlines, Spirit Airlines, SouthWest Airlines
+	ticker = ["AAL", "DAL", "SAVE", "LUV"]
+	start, end = '2016-01-01', '2017-12-31'
+	for tick in ticker: 
+		p = download(tick, start, end)
+		f = factors(start, end)
+		reg = LinearRegression().fit(f, p)
+		#Testing error
+		teststart, testend = '2018-01-01', '2018-12-31'
+		print(tick, loss(teststart, testend, tick, reg))
+
+def mainFn():
+	linearRegression()
+
+mainFn()
